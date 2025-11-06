01dac68a76dff72894cc8e26653d6eaaeca96309
Trillon008 <freego008@gmail.com>
2025-11-06 09:14:21 +0100

    some packages

--- distrobox/sre/packages.sh
+++ distrobox/sre/packages.sh
@@ -16,12 +16,12 @@ dnf install -y htop \
   bat
 
 # Sops
-curl -LO https://github.com/getsops/sops/releases/download/v3.9.0/sops-v3.9.0.linux.amd64
-mv sops-v3.9.0.linux.amd64 /bin/sops
+curl -LO https://github.com/getsops/sops/releases/download/v3.11.0/sops-v3.11.0.linux.amd64
+mv sops-v3.11.0.linux.amd64 /usr/local/bin/sops
 chmod +x /bin/sops
 
 # k9s
-curl -LO https://github.com/derailed/k9s/releases/download/v0.50.4/k9s_linux_amd64.rpm
+curl -LO https://github.com/derailed/k9s/releases/download/v0.50.16/k9s_linux_amd64.rpm
 rpm -i k9s_linux_amd64.rpm
 rm -f k9s_linux_amd64.rpm
 
