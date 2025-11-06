af7fead7512c5f64894893f1220e52701e675aa0
Trillon <freego008@gmail.com>
2025-11-06 08:26:44 +0000

    update to fedora 43 (#18)
    * update to fedora 43
    
    * some packages

--- distrobox/sre/packages.sh
+++ distrobox/sre/packages.sh
@@ -6,21 +6,22 @@ dnf install -y htop \
   bind-utils \
   azure-cli \
   python3-pip \
-  terraform-1.10.5 \
+  terraform-1.12.2 \
   kubectl \
   google-cloud-cli \
   google-cloud-cli-gke-gcloud-auth-plugin \
   jq \
+  yq \
   helm \
   bat
 
 # Sops
-curl -LO https://github.com/getsops/sops/releases/download/v3.9.0/sops-v3.9.0.linux.amd64
-mv sops-v3.9.0.linux.amd64 /bin/sops
+curl -LO https://github.com/getsops/sops/releases/download/v3.11.0/sops-v3.11.0.linux.amd64
+mv sops-v3.11.0.linux.amd64 /usr/local/bin/sops
 chmod +x /bin/sops
 
 # k9s
-curl -LO https://github.com/derailed/k9s/releases/download/v0.50.4/k9s_linux_amd64.rpm
+curl -LO https://github.com/derailed/k9s/releases/download/v0.50.16/k9s_linux_amd64.rpm
 rpm -i k9s_linux_amd64.rpm
 rm -f k9s_linux_amd64.rpm
 
