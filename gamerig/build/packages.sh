f34d9af3de2996f21a8b51308e50c5672b104296
Tbouillon <tristan.bouillon@inthememory.com>
2026-01-01 20:04:09 +0100

    small adjustment on gamerig

--- gamerig/build/packages.sh
+++ gamerig/build/packages.sh
@@ -6,14 +6,11 @@ rpm-ostree install \
   pavucontrol-qt \
   neovim \
   restic \
-  starship \
   bat \
   htop
 
 rpm-ostree uninstall \
   vim-enhanced \
-  vim-common \
-  vim-minimal \
   google-noto-sans-javanese-fonts \
   google-noto-sans-balinese-fonts \
   google-noto-sans-sundanese-fonts
--- gamerig/build/repos.sh
+++ gamerig/build/repos.sh
@@ -6,15 +6,15 @@ set -exou pipefail
 rm -rf /etc/yum.repos.d/google-chrome.repo "/etc/yum.repos/_copr:copr.fedorainfracloud.org:phracek:PyCharm.repo"
 
 # Starship repo
-cat <<EOF >/etc/yum.repos.d/starship.repo
-[copr:copr.fedorainfracloud.org:atim:starship]
-name=Copr repo for starship owned by atim
-baseurl=https://download.copr.fedorainfracloud.org/results/atim/starship/fedora-\$releasever-\$basearch/
-type=rpm-md
-skip_if_unavailable=True
-gpgcheck=1
-gpgkey=https://download.copr.fedorainfracloud.org/results/atim/starship/pubkey.gpg
-repo_gpgcheck=0
-enabled=1
-enabled_metadata=1
-EOF
+#cat <<EOF >/etc/yum.repos.d/starship.repo
+#[copr:copr.fedorainfracloud.org:atim:starship]
+#name=Copr repo for starship owned by atim
+#baseurl=https://download.copr.fedorainfracloud.org/results/atim/starship/fedora-\$releasever-\$basearch/
+#type=rpm-md
+#skip_if_unavailable=True
+#gpgcheck=1
+#gpgkey=https://download.copr.fedorainfracloud.org/results/atim/starship/pubkey.gpg
+#repo_gpgcheck=0
+#enabled=1
+#enabled_metadata=1
+#EOF
