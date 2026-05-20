f4ab876cfddc28897a07f7aba2f9a958f056c156
Trillon <freego008@gmail.com>
2026-05-20 14:00:40 +0000

    Upgrade to Fedora 44 (#26)
    Basically upgrade to fedora 44. Also, clean up a bunch of file since bootc made things easier

--- desktop/build/packages.sh
+++ desktop/build/packages.sh
@@ -2,29 +2,33 @@
 
 set -exou pipefail
 
+## Test new filter
+wget -qcO /usr/lib64/ladspa/libdeep.so  https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/libdeep_filter_ladspa-0.5.6-x86_64-unknown-linux-gnu.so
+
 dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
-rpm-ostree install \
-  wireguard-tools \
-  libatomic \
-  pavucontrol-qt \
-  neovim \
-  restic \
-  ripgrep \
-  starship \
-  mpv \
-  bat \
-  yt-dlp \
-  btop \
-  openfortivpn \
-  plasma-nm-fortisslvpn \
-  NetworkManager-fortisslvpn \
-  wezterm \
-  tlp \
-  tailscale \
-  zed
+dnf update -y && dnf5 install -y \
+ neovim \
+ restic \
+ ripgrep \
+ starship \
+ mpv \
+ bat \
+ yt-dlp \
+ btop \
+ openfortivpn \
+ plasma-nm-fortisslvpn \
+ NetworkManager-fortisslvpn \
+ wezterm \
+ tailscale \
+ zed
 
-rpm-ostree uninstall \
+#Useless now ?
+#   wireguard-tools \
+# libatomic \
+# tlp conflict with tuned
+
+dnf5 remove -y \
   firefox \
   firefox-langpacks \
   vim-enhanced \
@@ -34,16 +38,4 @@ rpm-ostree uninstall \
   google-noto-sans-balinese-fonts \
   google-noto-sans-sundanese-fonts
 
-# ## Download cursor cli
-# EXTRACT_DIR=/ctx/cursor-cli
-# DOWNLOAD_URL="https://downloads.cursor.com/lab/2026.01.17-d239e66/${OS}/${ARCH}/agent-cli-package.tar.gz"
-
-# mkdir -p "${EXTRACT_DIR}"
-# if curl -fSL --progress-bar "${DOWNLOAD_URL}" \
-#   | tar --strip-components=1 -xzf - -C "${TEMP_EXTRACT_DIR}"; then
-#   echo "Success"
-# else
-#     echo "Download failed. Please check your internet connection and try again."
-#     exit 1
-# fi
-# mv "${EXTRACT_DIR}" /bin/cur
+dnf5 autoremove && dnf5 clean all
