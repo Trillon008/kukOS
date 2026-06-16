28c413c7f72352f939dd1586c0b5005b6b1b012c
Trillon008 <freego008@gmail.com>
2026-06-16 10:53:53 +0200

    update packages for qemu

--- desktop/build/packages.sh
+++ desktop/build/packages.sh
@@ -3,27 +3,27 @@
 set -exou pipefail
 
 ## Test new filter
-wget -qcO /usr/lib64/ladspa/libdeep.so  https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/libdeep_filter_ladspa-0.5.6-x86_64-unknown-linux-gnu.so
+wget -qcO /usr/lib64/ladspa/libdeep.so https://github.com/Rikorose/DeepFilterNet/releases/download/v0.5.6/libdeep_filter_ladspa-0.5.6-x86_64-unknown-linux-gnu.so
 
 dnf5 install -y --nogpgcheck --repofrompath 'terra,https://repos.fyralabs.com/terra$releasever' terra-release
 
 dnf5 update -y && dnf5 install -y \
- bat \
- btop \
- mpv \
- neovim \
- NetworkManager-fortisslvpn \
- openfortivpn \
- plasma-nm-fortisslvpn \
- qemu \
- restic \
- ripgrep \
- starship \
- tailscale \
- wezterm \
- yt-dlp \
- zed
-
+  bat \
+  btop \
+  edk2-ovmf \
+  mpv \
+  neovim \
+  NetworkManager-fortisslvpn \
+  openfortivpn \
+  plasma-nm-fortisslvpn \
+  qemu \
+  restic \
+  ripgrep \
+  starship \
+  tailscale \
+  wezterm \
+  yt-dlp \
+  zed
 
 dnf5 remove -y \
   firefox \
